c880646031a1b0a6ed481c9d31ee6c660dc668fa
Sean Chapman <sean.b.chapman@gmail.com>
2014-09-23 21:40:03 +1200

    Added core tests

---
+++ AudioSwitcher.AudioApi.Tests/ControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using AudioSwitcher.Tests.Common;
+using Xunit;
+
+namespace AudioSwitcher.AudioApi.Tests
+{
+    public class ControllerTests
+    {
+        private AudioController CreateTestController()
+        {
+            return new TestAudioController(new TestDeviceEnumerator(2, 2));
+        }
+
+        [Fact]
+        public void Create_Controller()
+        {
+            var controller = CreateTestController();
+            Assert.NotNull(controller);
+        }
+
+        [Fact]
+        public void Controller_GetAllDevices()
+        {
+            var controller = CreateTestController();
+            Assert.NotEmpty(controller.GetAllDevices());
+            Assert.Equal(4, controller.GetAllDevices().Count());
+        }
+
+        [Fact]
+        public void Controller_GetAllPlaybackDevices()
+        {
+            var controller = CreateTestController();
+            Assert.NotEmpty(controller.GetPlaybackDevices());
+            Assert.Equal(2, controller.GetPlaybackDevices().Count());
+            Assert.True(controller.GetPlaybackDevices().All(x => x.IsPlaybackDevice));
+        }
+
+        [Fact]
+        public void Controller_GetAllCaptureDevices()
+        {
+            var controller = CreateTestController();
+            Assert.NotEmpty(controller.GetCaptureDevices());
+            Assert.Equal(2, controller.GetCaptureDevices().Count());
+            Assert.True(controller.GetCaptureDevices().All(x => x.IsCaptureDevice));
+        }
+
+    }
+}
