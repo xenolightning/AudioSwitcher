022fc37f7e58f5127a3188c26a0dfecb501cf963
Sean Chapman <sean.b.chapman@gmail.com>
2014-11-14 22:37:14 +1300

    Accidentally left out the bridge files. Will rewrite.

---
+++ AudioSwitcher.Scripting.JavaScript/Internal/ScriptOutputBridge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jurassic.Library;
+
+namespace AudioSwitcher.Scripting.JavaScript.Internal
+{
+    public class ScriptOutputBridge : IFirebugConsoleOutput
+    {
+        private readonly IScriptOutput _output;
+
+        public ScriptOutputBridge(IScriptOutput output)
+        {
+            _output = output;
+        }
+
+        public void Log(FirebugConsoleMessageStyle style, object[] objects)
+        {
+        }
+
+        public void Clear()
+        {
+        }
+
+        public void StartGroup(string title, bool initiallyCollapsed)
+        {
+        }
+
+        public void EndGroup()
+        {
+        }
+    }
+}
