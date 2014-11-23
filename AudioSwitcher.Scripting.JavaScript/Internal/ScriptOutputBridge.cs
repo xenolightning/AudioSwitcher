c6592001e06c905d9ec7f4a962fdf70214c07947
xenolightning <sean.b.chapman@gmail.com>
2014-11-24 12:05:43 +1300

    Script output

--- AudioSwitcher.Scripting.JavaScript/Internal/ScriptOutputBridge.cs
+++ AudioSwitcher.Scripting.JavaScript/Internal/ScriptOutputBridge.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Jurassic;
 using Jurassic.Library;
 
 namespace AudioSwitcher.Scripting.JavaScript.Internal
 {
-    public class ScriptOutputBridge : IFirebugConsoleOutput
+
+    /// <summary>
+    /// Represents an implementation of the Firebug API using the standard console.
+    /// </summary>
+    internal class ScriptOutputBridge : IFirebugConsoleOutput
     {
         private readonly IScriptOutput _output;
 
@@ -17,10 +19,32 @@ namespace AudioSwitcher.Scripting.JavaScript.Internal
 
         public void Log(FirebugConsoleMessageStyle style, object[] objects)
         {
+            // Convert the objects to a string.
+            var message = new System.Text.StringBuilder();
+            for (int i = 0; i < objects.Length; i++)
+            {
+                message.Append(' ');
+                message.Append(TypeConverter.ToString(objects[i]));
+            }
+
+            switch (style)
+            {
+                case FirebugConsoleMessageStyle.Regular:
+                case FirebugConsoleMessageStyle.Information:
+                case FirebugConsoleMessageStyle.Warning:
+                    _output.Log(message.ToString());
+                    break;
+                case FirebugConsoleMessageStyle.Error:
+                    _output.Error(message.ToString());
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("style");
+            }
         }
 
         public void Clear()
         {
+            _output.Clear();
         }
 
         public void StartGroup(string title, bool initiallyCollapsed)
