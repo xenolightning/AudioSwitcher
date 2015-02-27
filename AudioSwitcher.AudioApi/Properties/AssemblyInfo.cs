27cf071728735ed6b3275bbcee36b1ad8d85796d
xenolightning <sean.b.chapman@gmail.com>
2015-02-27 14:17:28 +1300

    Updated version numbers

--- AudioSwitcher.AudioApi/Properties/AssemblyInfo.cs
+++ AudioSwitcher.AudioApi/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.1.3.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyVersion("2.2.0.2702")]
+[assembly: AssemblyFileVersion("2.2.0.2702")]
