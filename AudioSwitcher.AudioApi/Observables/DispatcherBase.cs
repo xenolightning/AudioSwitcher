5d0e7f5f088c01749ed88eee3e642088617efa34
xenolightning <sean.b.chapman@gmail.com>
2015-09-10 21:47:27 +1200

    Intermediaries

---
+++ AudioSwitcher.AudioApi/Observables/DispatcherBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AudioSwitcher.AudioApi.Observables
+{
+    public abstract class DispatcherBase<T> : IDispatcher<T>, IDisposable
+    {
+        public abstract bool HasObservers { get; }
+
+        public abstract bool IsDisposed { get; }
+
+        public abstract void OnNext(T value);
+
+        public abstract void OnError(Exception error);
+
+        public abstract void OnCompleted();
+
+        public abstract IDisposable Subscribe(IObserver<T> observer);
+
+        public abstract void Dispose();
+    }
+}
