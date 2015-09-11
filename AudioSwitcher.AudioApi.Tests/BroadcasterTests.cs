df95e24030b18eb2911d00aa58f4f8b45e055af8
xenolightning <sean.b.chapman@gmail.com>
2015-09-11 14:08:42 +1200

    Some broadcaster tests

---
+++ AudioSwitcher.AudioApi.Tests/BroadcasterTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using AudioSwitcher.AudioApi.Observables;
+using Moq;
+using Xunit;
+
+namespace AudioSwitcher.AudioApi.Tests
+{
+    public class BroadcasterTests
+    {
+        [Fact]
+        public void Broacaster_Subscribe()
+        {
+            var b = new Broadcaster<int>();
+
+            var sub = b.Subscribe(x => { });
+
+            Assert.NotNull(sub);
+        }
+
+        [Fact]
+        public void Broacaster_OnNext()
+        {
+            var b = new Broadcaster<int>();
+
+            int result = -1;
+
+            var sub = b.Subscribe(x => { result = x; });
+
+            Assert.NotNull(sub);
+
+            b.OnNext(2);
+
+            Assert.NotEqual(-1, result);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Broacaster_Subscribe_SubscriptionDispose()
+        {
+            var b = new Broadcaster<int>();
+
+            var sub = b.Subscribe(x => { });
+
+            Assert.NotNull(sub);
+
+            Assert.True(b.HasObservers);
+
+            sub.Dispose();
+
+            Assert.False(b.HasObservers);
+        }
+
+        [Fact]
+        public void Broacaster_DisposedSubscription_OnNext()
+        {
+            var b = new Broadcaster<int>();
+
+            int result = -1;
+
+            var sub = b.Subscribe(x => { result = x; });
+
+            Assert.NotNull(sub);
+            sub.Dispose();
+
+            b.OnNext(2);
+
+            Assert.NotEqual(2, result);
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void Broacaster_Disposed_OnNext()
+        {
+            var b = new Broadcaster<int>();
+
+            int result = -1;
+
+            var sub = b.Subscribe(x => { result = x; });
+
+            Assert.NotNull(sub);
+            sub.Dispose();
+
+            b.Dispose();
+
+            b.OnNext(2);
+
+            Assert.NotEqual(2, result);
+            Assert.Equal(-1, result);
+        }
+
+        [Fact]
+        public void Broacaster_OnCompleted()
+        {
+            var b = new Broadcaster<int>();
+
+            bool complete = false;
+
+            var sub = b.Subscribe(x => { }, () => { complete = true; });
+
+            Assert.NotNull(sub);
+
+            b.OnCompleted();
+
+            Assert.True(complete);
+        }
+
+        [Fact]
+        public void Broacaster_Dispose_OnCompleted()
+        {
+            var b = new Broadcaster<int>();
+
+            bool complete = false;
+
+            var sub = b.Subscribe(x => { }, () => { complete = true; });
+
+            Assert.NotNull(sub);
+
+            b.Dispose();
+
+            Assert.True(complete);
+        }
+
+        [Fact]
+        public void Broacaster_DisposedSubscription_OnCompleted()
+        {
+            var b = new Broadcaster<int>();
+
+            bool complete = false;
+
+            var sub = b.Subscribe(x => { }, () => { complete = true; });
+
+            Assert.NotNull(sub);
+
+            sub.Dispose();
+
+            //disposing sub should not fire on completed
+            Assert.False(complete);
+        }
+
+        [Fact]
+        public void Broacaster_OnError_FromOnNext()
+        {
+            var b = new Broadcaster<int>();
+
+            var exception = new Exception("HAI");
+            Exception result = null;
+
+            var sub = b.Subscribe(x => { throw exception; }, x => result = x);
+
+            Assert.NotNull(sub);
+
+            b.OnNext(1);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(exception, result);
+        }
+
+        [Fact]
+        public void Broacaster_OnError_FromOnError()
+        {
+            var b = new Broadcaster<int>();
+
+            var exception = new Exception("HAI");
+            Exception result = null;
+
+            var sub = b.Subscribe(x => { }, x => result = x);
+
+            Assert.NotNull(sub);
+
+            b.OnError(exception);
+
+            Assert.NotNull(result);
+
+            Assert.Equal(exception, result);
+        }
+
+    }
+}
--- AudioSwitcher.AudioApi/Observables/ObservableExtensions.cs
+++ AudioSwitcher.AudioApi/Observables/ObservableExtensions.cs
@@ -12,6 +12,21 @@ namespace AudioSwitcher.AudioApi.Observables
             return observable.Subscribe(new DelegateObserver<T>(onNext));
         }
 
+        public static IDisposable Subscribe<T>(this IObservable<T> observable, Action<T> onNext, Action<Exception> onError)
+        {
+            return observable.Subscribe(new DelegateObserver<T>(onNext, onError));
+        }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> observable, Action<T> onNext, Action onCompleted)
+        {
+            return observable.Subscribe(new DelegateObserver<T>(onNext, onCompleted));
+        }
+
+        public static IDisposable Subscribe<T>(this IObservable<T> observable, Action<T> onNext, Action<Exception> onError, Action onCompleted)
+        {
+            return observable.Subscribe(new DelegateObserver<T>(onNext, onError, onCompleted));
+        }
+
         public static IObservable<T> AsObservable<T>(this IObservable<T> observable)
         {
             if (observable == null) throw new ArgumentNullException("observable");
