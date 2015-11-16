40a25993d26b3eaaab86fae05402a2fa9d1b4a9c
xenolightning <sean.b.chapman@gmail.com>
2015-11-17 10:43:33 +1300

    Updated projects to reference projects instead of nuget where applicable.
    Updated copyright year

--- AudioSwitcher.Scripting.JavaScript/Properties/AssemblyInfo.cs
+++ AudioSwitcher.Scripting.JavaScript/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AudioSwitcher.Scripting.JavaScriptScript")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
